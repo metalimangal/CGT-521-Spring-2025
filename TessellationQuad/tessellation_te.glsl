051660d5dc456255782769e1a7b77bdd49e7ce64
Metali Mangal <31962950+metalimangal@users.noreply.github.com>
2025-04-09 19:04:57 -0400

    Update tessellation_te.glsl

--- TessellationQuad/tessellation_te.glsl
+++ TessellationQuad/tessellation_te.glsl
@@ -1,24 +1,17 @@
-#version 430 
+#version 430
 
-//layout (quads, equal_spacing, ccw) in;
-//Try some of these other options
-//layout (quads, fractional_odd_spacing, ccw) in;
-layout (quads, fractional_even_spacing, ccw) in;
+layout(triangles, fractional_odd_spacing, ccw) in;
 
 
 void main()
 {
-	const float u = gl_TessCoord[0];
-	const float v = gl_TessCoord[1];
+    // Calculate the tessellated position of the fragment
+    vec3 p0 = gl_in[0].gl_Position.xyz;
+    vec3 p1 = gl_in[1].gl_Position.xyz;
+    vec3 p2 = gl_in[2].gl_Position.xyz;
 
-	const vec4 p0 = gl_in[0].gl_Position;
-	const vec4 p1 = gl_in[1].gl_Position;
-	const vec4 p2 = gl_in[2].gl_Position;
-	const vec4 p3 = gl_in[3].gl_Position;
+    // Barycentric interpolation for the tessellated triangle
+    vec3 p = (1.0 - gl_TessCoord.x - gl_TessCoord.y) * p0 + gl_TessCoord.x * p1 + gl_TessCoord.y * p2;
 
-	vec4 a = mix(p0, p1, u);
-	vec4 b = mix(p3, p2, u);
-	vec4 c = mix(a, b, v);
-
-	gl_Position = c;
+    gl_Position = vec4(p, 1.0);  // Set the final position
 }
